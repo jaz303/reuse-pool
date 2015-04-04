63252e110834401aa4897cb2db5ce6cd55585948
Jason Frame <jason@onehackoranother.com>
2015-04-04 12:50:09 +0100

    tabs -> spaces

--- index.js
+++ index.js
@@ -3,26 +3,26 @@ var NO_OP = function() {};
 
 module.exports = reusePool;
 function reusePool(factory, opts) {
-	return new ReusePool(factory, opts);
+    return new ReusePool(factory, opts);
 }
 
 function ReusePool(factory, opts) {
-	this._factory = factory;
-	this._recycled = [];
-	opts = opts || EMPTY;
-	this._prepare = opts.prepare || NO_OP;
+    this._factory = factory;
+    this._recycled = [];
+    opts = opts || EMPTY;
+    this._prepare = opts.prepare || NO_OP;
 }
 
 ReusePool.prototype.get = function() {
-	if (this._recycled.length) {
-		var obj = this._recycled.pop();
-		this._prepare(obj);
-		return obj;
-	} else {
-		return this._factory();
-	}
+    if (this._recycled.length) {
+        var obj = this._recycled.pop();
+        this._prepare(obj);
+        return obj;
+    } else {
+        return this._factory();
+    }
 }
 
 ReusePool.prototype.recycle = function(obj) {
-	this._recycled.push(obj);
+    this._recycled.push(obj);
 }
--- test/all.js
+++ test/all.js
@@ -3,59 +3,59 @@ var pool = require('../');
 
 test('get returns new object when pool is empty', function(assert) {
 
-	var p = pool(function() { return "foo"; });
-	var obj = p.get();
+    var p = pool(function() { return "foo"; });
+    var obj = p.get();
 
-	assert.equals(obj, "foo");
-	assert.end();
+    assert.equals(obj, "foo");
+    assert.end();
 
 });
 
 test('get returns pooled object when available', function(assert) {
 
-	var p = pool(function() { return {}; });
+    var p = pool(function() { return {}; });
 
-	var obj1 = p.get();
-	var obj2 = p.get();
+    var obj1 = p.get();
+    var obj2 = p.get();
 
-	p.recycle(obj1);
-	p.recycle(obj2);
+    p.recycle(obj1);
+    p.recycle(obj2);
 
-	assert.equal(p.get(), obj2);
-	assert.equal(p.get(), obj1);
-	assert.end();
+    assert.equal(p.get(), obj2);
+    assert.equal(p.get(), obj1);
+    assert.end();
 
 });
 
 test('prepare is not called before returning new object', function(assert) {
 
-	var p = pool(function() { return {}; }, {
-		prepare: function(obj) {
-			obj.ready = true;
-		}
-	});
+    var p = pool(function() { return {}; }, {
+        prepare: function(obj) {
+            obj.ready = true;
+        }
+    });
 
-	var obj = p.get();
+    var obj = p.get();
 
-	assert.ok(!obj.ready);
-	assert.end();
+    assert.ok(!obj.ready);
+    assert.end();
 
 
 });
 
 test('prepare is called before returning reused object', function(assert) {
 
-	var p = pool(function() { return {}; }, {
-		prepare: function(obj) {
-			obj.ready = true;
-		}
-	});
+    var p = pool(function() { return {}; }, {
+        prepare: function(obj) {
+            obj.ready = true;
+        }
+    });
 
-	var obj = p.get();
-	p.recycle(obj);
-	obj = p.get();
+    var obj = p.get();
+    p.recycle(obj);
+    obj = p.get();
 
-	assert.ok(obj.ready);
-	assert.end();
+    assert.ok(obj.ready);
+    assert.end();
 
 });
